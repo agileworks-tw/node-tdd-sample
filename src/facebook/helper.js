199209e8047e16869bdfd62eb84610da5fe6b230
smlsunxie <smlsun@gmail.com>
2016-07-19 14:26:03 +0800

    use es6

--- src/facebook/helper.js
+++ src/facebook/helper.js
@@ -1,12 +1,37 @@
-var FB, exports;
+import FB from 'fb'
 
-FB = require("fb");
+// export default (userid, token, cb) => {
+//   FB.setAccessToken(token);
+//   return FB.api(userid + '/friends', function(res) {
+//     return cb(null, res.data);
+//   });
+// };
 
-exports = module.exports = {};
+export default class FacebookHelper {
 
-exports.getFriends = function(userid, token, cb) {
-  FB.setAccessToken(token);
-  return FB.api(userid + '/friends', function(res) {
-    return cb(null, res.data);
-  });
-};
+  constructor({token, userId}) {
+    this.FB = FB;
+    this.FB.setAccessToken(token);
+    this.userId = userId;
+  }
+
+  async getFriends() {
+    try {
+      console.log('this.userId', this.userId);
+      let result = await new Promise((resolve, reject) => {
+        this.FB.api(this.userId + '/friends', function(res, error) {
+          if(error) reject(error);
+          resolve(res.data);
+        });
+      });
+      return result;
+
+    } catch (e) {
+      throw e;
+    }
+
+
+
+  }
+
+}
